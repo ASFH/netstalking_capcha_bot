9d6d7be0e383fc8692120906f392983a3ec26c1e
ASFH <karpechenkov.k@gmail.com>
2019-11-12 20:26:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,19 @@
-from config import TOKEN, TESTS
+from config import TOKEN, TESTS, chat_for_capcha_bot, asdasda
 import telebot
 import time
 import random
 import threading
 
-CHAT_ID = -1001443189124
-CHAT_ID2 = -1001446867417
+CHAT_ID = asdasda
 
 # dict {uid: [msg_id, ..]}
 UNSAFE_MESSAGES = dict()
-CAPTCHA_TIMEOUT = 5 # seconds
+CAPTCHA_TIMEOUT = 10 # seconds
 LIMIT = 5
 
 bot = telebot.TeleBot(TOKEN)
 
-def kick_user(uid):
+def kick_user(uid, msg_from_bot):
     """
         executes in separate thread;
         kicks user and removes its messages after timeout if it didn't pass the exam
@@ -22,12 +21,16 @@ def kick_user(uid):
     time.sleep(CAPTCHA_TIMEOUT)
     if uid in UNSAFE_MESSAGES:
         bot.kick_chat_member(CHAT_ID, uid)
+        kicked_user_id = bot.get_updates()[-1].message.message_id
+        bot.delete_message(CHAT_ID, kicked_user_id)
+        print('removing message {0} from bot'.format(kicked_user_id))
         bot.unban_chat_member(CHAT_ID, uid)
         for msg_id in UNSAFE_MESSAGES[uid]:
             print("removing message", msg_id, "from user", uid)
             bot.delete_message(chat_id=CHAT_ID, message_id=msg_id)
+        bot.delete_message(CHAT_ID, msg_from_bot)
+        print('removing message {0} from bot'.format(msg_from_bot))
         del(UNSAFE_MESSAGES[uid])
-        print(UNSAFE_MESSAGES)
 
 def show_captcha_keyboard():
     """
@@ -57,21 +60,21 @@ def on_user_joins(m):
         sends notification message to each joined user and triggers `kick_user`
     """
     def _gen_captcha_text(name):
-        _captcha_text = ("@%s, please, press the button below within the time amount"
-                         "specified, otherwise you will be kicked. Thank you! (%s sec)")
+        _captcha_text = ("@{0}, please, press the button below within the time amount"
+                         " specified, otherwise you will be kicked. Thank you! ({1} sec)")
         return _captcha_text.format(name, CAPTCHA_TIMEOUT)
     if m.from_user.id not in UNSAFE_MESSAGES: 
-        UNSAFE_MESSAGES[m.from_user.id] = []
-        #TODO: change `x` to some valuable name
-        x = threading.Thread(target=kick_user, args=(m.from_user.id, ))
-        x.start()
+        UNSAFE_MESSAGES[m.from_user.id] = [m.message_id]
         name = m.from_user.username if m.from_user.username != None else m.from_user.first_name
-        bot.send_message(
+        msg_from_bot = bot.send_message(
             CHAT_ID,
             _gen_captcha_text(name),
             parse_mode='HTML',
             reply_markup=show_captcha_keyboard()
         )
+        #print(msg_from_bot)
+        thread = threading.Thread(target=kick_user, args=(m.from_user.id, msg_from_bot.message_id))
+        thread.start()
 
 @bot.callback_query_handler(func=lambda message:True)
 def answer(message):
@@ -83,8 +86,7 @@ def answer(message):
         del(UNSAFE_MESSAGES[message.from_user.id])
         print(UNSAFE_MESSAGES)
 
-#FIXME: consider switching to any content type since spam messages may have not only text data?
-@bot.message_handler(content_types=['text'])
+@bot.message_handler(content_types=['text', 'photo', 'video', 'document', 'audio', 'animation', 'voice', 'sticker'])
 def get_user_messages(message):
     """
         processes new messages; removes those which exceed predefined LIMIT
@@ -93,9 +95,6 @@ def get_user_messages(message):
         if len(UNSAFE_MESSAGES[message.from_user.id]) >= LIMIT:
             bot.delete_message(chat_id=CHAT_ID, message_id=message.message_id)
         else:
-            #FIXME: why not `UNSAFE_MESSAGES[message.from_user.id].append(message.message_id)` ?
-            temp1 = UNSAFE_MESSAGES[message.from_user.id]
-            temp1.append(message.message_id)
-            UNSAFE_MESSAGES[message.from_user.id] = temp1
+            UNSAFE_MESSAGES[message.from_user.id].append(message.message_id)
 
 bot.polling()
