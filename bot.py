6c16a7d497b1f80ab28bd983a676d5c803beacde
ASFH <karpechenkov.k@gmail.com>
2019-11-13 19:00:28 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,15 +5,21 @@ import random
 import threading
 from tinydb import TinyDB, Query
 
+db = TinyDB('users.json')
+User = Query()
+bot = telebot.TeleBot(TOKEN)
+
 CHAT_ID = asdasda
 # dict {uid: [msg_id, ..]}
 UNSAFE_MESSAGES = dict()
 CAPTCHA_TIMEOUT = 20 # seconds
 LIMIT = 5
+ADMINS = list()
 
-db = TinyDB('users.json')
-User = Query()
-bot = telebot.TeleBot(TOKEN)
+for i in bot.get_chat_administrators(CHAT_ID):
+    ADMINS.append(i.user.id)
+
+print(ADMINS)
 
 def kick_user(uid, msg_from_bot, first_name):
     """
@@ -46,31 +52,40 @@ def cleanup_messages(message):
         triggered manually, it cleanups all UNSAFE_MESSAGES
     """
     print(UNSAFE_MESSAGES)
-    if UNSAFE_MESSAGES:
-        for uid, messages in UNSAFE_MESSAGES.items():
-            for msg_id in messages:
-                print("removing message", msg_id, "from user", uid)
-                bot.delete_message(chat_id=CHAT_ID, message_id=msg_id)
+    if message.from_user.id in ADMINS:
+        if UNSAFE_MESSAGES:
+            for uid, messages in UNSAFE_MESSAGES.items():
+                for msg_id in messages:
+                    print("removing message", msg_id, "from user", uid)
+                    bot.delete_message(chat_id=CHAT_ID, message_id=msg_id)
+        else:
+            bot.send_message(chat_id=CHAT_ID, text="Нечего чистить")
     else:
-        bot.send_message(chat_id=CHAT_ID, text="Нечего чистить")
+        if message.from_user.id in UNSAFE_MESSAGES:
+            if len(UNSAFE_MESSAGES[message.from_user.id]) >= LIMIT:
+                bot.delete_message(chat_id=CHAT_ID, message_id=message.message_id)
+            else:
+                UNSAFE_MESSAGES[message.from_user.id].append(message.message_id)
+        
 
 @bot.message_handler(func=lambda m: True, content_types=['new_chat_members'])
 def on_user_joins(m):
     """
         sends notification message to each joined user and triggers `kick_user`
     """
-    def _gen_captcha_text(name):
-        _captcha_text = ("@{0}, please, press the button below within the time amount"
-                         " specified, otherwise you will be kicked. Thank you! ({1} sec)")
-        return _captcha_text.format(name, CAPTCHA_TIMEOUT)
+    def _gen_captcha_text(name, uid):
+        _captcha_text = ("[{0}](tg://user?id={1}), please, press the button below within the time amount"
+                         " specified, otherwise you will be kicked. Thank you! ({2} sec)")
+        return _captcha_text.format(name, uid, CAPTCHA_TIMEOUT)
     if len(db.search(User.user_id == m.from_user.id)) == 0:
         if m.from_user.id not in UNSAFE_MESSAGES: 
             UNSAFE_MESSAGES[m.from_user.id] = [m.message_id]
-            name = m.from_user.username if m.from_user.username != None else m.from_user.first_name
+            name = m.from_user.first_name if m.from_user.first_name != None else m.from_user.username
+            uid = m.from_user.id
             msg_from_bot = bot.send_message(
                 CHAT_ID,
-                _gen_captcha_text(name),
-                parse_mode='HTML',
+                _gen_captcha_text(name, uid),
+                parse_mode='Markdown',
                 reply_markup=show_captcha_keyboard()
             )
         thread = threading.Thread(target=kick_user, args=(m.from_user.id, msg_from_bot.message_id, m.from_user.first_name, ))
@@ -85,10 +100,15 @@ def answer(message):
         bot.delete_message(CHAT_ID, message.message.message_id)
         del(UNSAFE_MESSAGES[message.from_user.id])
         print(UNSAFE_MESSAGES)
-        db.insert({'user_id': message.from_user.id, 'username': message.from_user.username, 'first_name': message.from_user.first_name})
+        db.insert(
+            {'user_id': message.from_user.id, 
+            'username': message.from_user.username, 
+            'first_name': message.from_user.first_name, 
+            'last_name': message.from_user.last_name}
+        )
         print('User {0} created'.format(db.search(User.first_name == message.from_user.first_name)[0]['first_name']))
 
-@bot.message_handler(content_types=['text', 'photo', 'video', 'document', 'audio', 'animation', 'voice', 'sticker'])
+@bot.message_handler(content_types=['text', 'photo', 'video', 'document', 'audio', 'animation', 'voice', 'sticker', 'bot_command'])
 def get_user_messages(message):
     """
         processes new messages; removes those which exceed predefined LIMIT
